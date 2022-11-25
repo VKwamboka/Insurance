28c2186e2d2892463d4cf6a9fd338a2e502447ca
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-25 21:57:56 +0300

    updated style

--- src/components/pricing/Price.js
+++ src/components/pricing/Price.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Header from '../header/Header'
 
 export default function Price() {
   return (
     <div>
-
+        <Header/>
     <div className="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div className="container py-5">
-            <div className="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+            <div className="section-title text-center position-relative pb-3 mb-5 mx-auto" style={{maxWidth: "600px"}}>
                 <h5 className="fw-bold text-primary text-uppercase">Pricing Plans</h5>
                 <h1 className="mb-0">We are Offering Competitive Prices for Our Clients</h1>
             </div>
@@ -19,8 +20,8 @@ export default function Price() {
                         </div>
                         <div className="p-5 pt-0">
                             <h1 className="display-5 mb-3">
-                                <small className="align-top" style="font-size: 22px; line-height: 45px;">$</small>49.00<small
-                                    className="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
+                                <small className="align-top" style={{fontSize: "22px", lineHeight: "45px"}}>$</small>49.00<small
+                                    className="align-bottom" style={{fontSize: "16px", lineHeight: "40px"}}>/ Month</small>
                             </h1>
                             <div className="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i className="fa fa-check text-primary pt-1"></i></div>
                             <div className="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i className="fa fa-check text-primary pt-1"></i></div>
@@ -31,15 +32,15 @@ export default function Price() {
                     </div>
                 </div>
                 <div className="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
-                    <div className="bg-white rounded shadow position-relative" style="z-index: 1;">
+                    <div className="bg-white rounded shadow position-relative" style={{zIndex: 1}}>
                         <div className="border-bottom py-4 px-5 mb-4">
                             <h4 className="text-primary mb-1">Standard Plan</h4>
                             <small className="text-uppercase">For Medium Size Business</small>
                         </div>
                         <div className="p-5 pt-0">
                             <h1 className="display-5 mb-3">
-                                <small className="align-top" style="font-size: 22px; line-height: 45px;">$</small>99.00<small
-                                    className="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
+                                <small className="align-top" style={{fontSize: ",", lineHeight: "45px"}}>$</small>99.00<small
+                                    className="align-bottom" style={{fontSize: "16px", lineHeight: "40px"}}>/ Month</small>
                             </h1>
                             <div className="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i className="fa fa-check text-primary pt-1"></i></div>
                             <div className="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i className="fa fa-check text-primary pt-1"></i></div>
@@ -57,8 +58,8 @@ export default function Price() {
                         </div>
                         <div className="p-5 pt-0">
                             <h1 className="display-5 mb-3">
-                                <small className="align-top" style="font-size: 22px; line-height: 45px;">$</small>149.00<small
-                                    className="align-bottom" style="font-size: 16px; line-height: 40px;">/ Month</small>
+                                <small className="align-top" style={{fontSize: "22px", lineHeight: "45px"}}>$</small>149.00<small
+                                    className="align-bottom" style={{fontSize: "16px", lineHeight: "40px"}}>/ Month</small>
                             </h1>
                             <div className="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i className="fa fa-check text-primary pt-1"></i></div>
                             <div className="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i className="fa fa-check text-primary pt-1"></i></div>
