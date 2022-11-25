7588bf4c7d2f29433f730c8ebaa04117ad6b3762
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-25 22:50:50 +0300

    updated style

--- src/components/pricing/Price.js
+++ src/components/pricing/Price.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../header/Header'
-import './style.css' 
-import './bootstrap.min.css'
+// import './style.css' 
+// import './bootstrap.min.css'
 
 export default function Price() {
   return (
+    <main>
     <div>
-        <Header/>
+        <div>
+            <Header/>
+        </div>
+       
     <div className="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div className="container py-5">
             <div className="section-title text-center position-relative pb-3 mb-5 mx-auto" style={{maxWidth: "600px"}}>
@@ -17,26 +21,26 @@ export default function Price() {
                 <div className="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
                     <div className="bg-light rounded">
                         <div className="border-bottom py-4 px-5 mb-4">
-                            <h4 className="text-primary mb-1">Family Plan</h4>
-                            <small className="text-uppercase">For Small Size Business</small>
+                            <h4 className="text-primary mb-1">Basic Plan</h4>
+                            {/* <small className="text-uppercase">For Small Size Business</small> */}
                         </div>
                         <div className="p-5 pt-0">
                             <h1 className="display-5 mb-3">
                                 <small className="align-top" style={{fontSize: "22px", lineHeight: "45px"}}>$</small>49.00<small
                                     className="align-bottom" style={{fontSize: "16px", lineHeight: "40px"}}>/ Month</small>
                             </h1>
-                            <div className="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-3"><span>Responsive Layout</span><i className="fa fa-times text-danger pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-2"><span>Cross-browser Support</span><i className="fa fa-times text-danger pt-1"></i></div>
-                            <a href="" className="btn btn-primary py-2 px-4 mt-4">Order Now</a>
+                            <div className="d-flex justify-content-between mb-3"><span> Inpatient</span><i class="fa fa-check text-primary pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-3"><span>Outpatient</span><i class="fa fa-check text-primary pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-3"><span>Cancer care</span><i class="fa fa-times text-danger pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-2"><span>Family Health Cover </span><i class="fa fa-times text-danger pt-1"></i></div>
+                            <a href="" className="btn btn-primary py-2 px-4 mt-4">Insure Now</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div className="bg-white rounded shadow position-relative" style={{zIndex: 1}}>
                         <div className="border-bottom py-4 px-5 mb-4">
-                            <h4 className="text-primary mb-1">Individual Plan</h4>
+                            <h4 className="text-primary mb-1">Standard Plan</h4>
                             <small className="text-uppercase">For Medium Size Business</small>
                         </div>
                         <div className="p-5 pt-0">
@@ -44,18 +48,18 @@ export default function Price() {
                                 <small className="align-top" style={{fontSize: ",", lineHeight: "45px"}}>$</small>99.00<small
                                     className="align-bottom" style={{fontSize: "16px", lineHeight: "40px"}}>/ Month</small>
                             </h1>
-                            <div className="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-3"><span>Responsive Layout</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-2"><span>Cross-browser Support</span><i className="fa fa-times text-danger pt-1"></i></div>
-                            <a href="" className="btn btn-primary py-2 px-4 mt-4">Order Now</a>
+                            <div className="d-flex justify-content-between mb-3"><span> Inpatient</span><i class="fa fa-check text-primary pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-3"><span>Outpatient</span><i class="fa fa-check text-primary pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-3"><span>Cancer care</span><i class="fa fa-times text-danger pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-2"><span>Family Health Cover </span><i class="fa fa-times text-danger pt-1"></i></div>
+                            <a href="" className="btn btn-primary py-2 px-4 mt-4">Insure Now</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
                     <div className="bg-light rounded">
                         <div className="border-bottom py-4 px-5 mb-4">
-                            <h4 className="text-primary mb-1">Group Plan</h4>
+                            <h4 className="text-primary mb-1">Advanced Plan</h4>
                             <small className="text-uppercase">For Large Size Business</small>
                         </div>
                         <div className="p-5 pt-0">
@@ -63,11 +67,11 @@ export default function Price() {
                                 <small className="align-top" style={{fontSize: "22px", lineHeight: "45px"}}>$</small>149.00<small
                                     className="align-bottom" style={{fontSize: "16px", lineHeight: "40px"}}>/ Month</small>
                             </h1>
-                            <div className="d-flex justify-content-between mb-3"><span>HTML5 & CSS3</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-3"><span>Bootstrap v5</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-3"><span>Responsive Layout</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <div className="d-flex justify-content-between mb-2"><span>Cross-browser Support</span><i className="fa fa-check text-primary pt-1"></i></div>
-                            <a href="" className="btn btn-primary py-2 px-4 mt-4">Order Now</a>
+                            <div className="d-flex justify-content-between mb-3"><span> Inpatient</span><i class="fa fa-check text-primary pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-3"><span>Outpatient</span><i class="fa fa-check text-primary pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-3"><span>Cancer care</span><i class="fa fa-times text-danger pt-1"></i></div>
+                            <div className="d-flex justify-content-between mb-2"><span>Family Health Cover </span><i class="fa fa-times text-danger pt-1"></i></div>
+                            <a href="" className="btn btn-primary py-2 px-4 mt-4">Insure Now</a>
                         </div>
                     </div>
                 </div>
@@ -76,5 +80,6 @@ export default function Price() {
     </div>
     
     </div>
+    </main>
   )
 }
