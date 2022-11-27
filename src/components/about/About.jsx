188baa913a4d64a8867b102366b29234dfe1a623
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-27 22:26:26 +0300

    updated pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,10 +7,7 @@ export default function About() {
         <div>
             <Hero/>
         </div>
-     
-    <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div className="spinner"></div>
-    </div>
+  
   
     <div className="container-fluid position-relative p-0">
         <nav className="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0">
