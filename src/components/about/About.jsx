7463f27f745b7a0327642a1e48f93994f4730113
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-28 00:01:24 +0300

    updated pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../header/Header'
 import Hero from '../pricing/Heros'
 
+
 export default function About() {
   return (
     <div>
@@ -12,144 +13,7 @@ export default function About() {
             <Hero/>
         </div>
 
-        <section className="contact-section">
-      <div className="container">
-        {/* <ToastContainer position="top-center" /> */}
-        <div className="row justify-content-center">
-          <div className="col-md-10">
-            <div className="wrapper">
-              <div className="row no-gutters">
-                <div className="col-md-6">
-                  <div className="contact-wrap w-100 p-lg-5 p-4">
-                    <h3 className="mb-4">Send us a message</h3>
-                    <form
-                      id="contactForm"
-                      className="contactForm"
-                     
-                    >
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <input
-                              type="text"
-                              className="form-control"
-                              name="name"
-                              placeholder="Name"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <input
-                              type="email"
-                              className="form-control"
-                              name="email"
-                              placeholder="Email"
-                             
-                       
-                            />
-                          </div>
-                        </div>
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <input
-                              type="text"
-                              className="form-control"
-                              name="subject"
-                              placeholder="Subject"
-                             
-                            
-                            />
-                          </div>
-                        </div>
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <textarea
-                              type="text"
-                              className="form-control"
-                              name="message"
-                              placeholder="Message"
-                              cols="30"
-                              rows="6"
-                             
-                              
-                            ></textarea>
-                          </div>
-                        </div>
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <input
-                              type="submit"
-                              value="Send Message"
-                              className="btn btn-primary"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-                <div className="col-md-6 d-flex align-items-stretch">
-                  <div className="info-wrap w-100 p-lg-5 p-4 img">
-                    <h3>Contact us</h3>
-                    <p className="mb-4">
-                      We're open for any suggestion or just to have a chat
-                    </p>
-                    <div className="dbox w-100 d-flex align-items-start">
-                      <div className="icon d-flex align-items-center justify-content-center">
-                        <span className="fa fa-map-marker"></span>
-                      </div>
-                      <div className="text pl-3">
-                        <p>
-                          <span>Address:</span> 198 West 21th Street, Suite 721
-                          New York NY 10016
-                        </p>
-                      </div>
-                    </div>
-                    <div className="dbox w-100 d-flex align-items-center">
-                      <div className="icon d-flex align-items-center justify-content-center">
-                        <span className="fa fa-phone"></span>
-                      </div>
-                      <div className="text pl-3">
-                        <p>
-                          <span>Phone:</span>
-                          <a href="tel://123456789">+1235 2355 98</a>
-                        </p>
-                      </div>
-                    </div>
-                    <div className="dbox w-100 d-flex align-items-center">
-                      <div className="icon d-flex align-items-center justify-content-center">
-                        <span className="fa fa-paper-plane"></span>
-                      </div>
-                      <div className="text pl-3">
-                        <p>
-                          <span>Email:</span>
-                          <a href="mailto:info@yoursite.com">
-                            info@yoursite.com
-                          </a>
-                        </p>
-                      </div>
-                    </div>
-                    <div className="dbox w-100 d-flex align-items-center">
-                      <div className="icon d-flex align-items-center justify-content-center">
-                        <span className="fa fa-globe"></span>
-                      </div>
-                      <div className="text pl-3">
-                        <p>
-                          <span>Website:</span>
-                          <a href="#">yoursite.com</a>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  
+        
     
     </div>
    
