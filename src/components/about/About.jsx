cd0572f9ce916d891e670e21d69c5910124d0983
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-27 23:31:00 +0300

    updated pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../header/Header'
 import Hero from '../pricing/Heros'
 
 export default function About() {
   return (
     <div>
+        <div>
+            <Header/>
+        </div>
         <div>
             <Hero/>
         </div>
